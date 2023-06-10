959a039be47615758203e829aafead1810672cae
Anna Haig <anna.haig@protonmail.com>
2023-06-10 20:20:10 +0100

    working

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,37 +1,33 @@
-use wasmer::{imports, Instance, Module, Store, Value};
+use wasmer::{wat2wasm, Module, Store};
 use wasmer_wasix::WasiEnv;
 
 fn main() {
-    let wasm_bytes = r#"
-    (module
-    (type $t0 (func (param i32) (result i32)))
-    (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
-        get_local $p0
-        i32.const 1
-        i32.add))
-    "#;
+    let wasm_bytes = wat2wasm(
+        br#"
+(module
+    (memory (export "memory") 1)
+  (type $add_one_t (func (param i32) (result i32)))
+  (func $add_one_f (type $add_one_t) (param $value i32) (result i32)
+    local.get $value
+    i32.const 1
+    i32.add)
+  (export "add_one" (func $add_one_f)))
+"#,
+    )
+    .unwrap();
 
     let mut store = Store::default();
 
     let module = Module::new(&store, wasm_bytes).unwrap();
 
-    // let wasi_env = WasiState::new("test-program")
-    //     .args(&["sum", "1", "2"])
-    //     .finalize()
-    //     .unwrap();
-
-    // let mut wasi_thread = wasi_env.import_object(store, module).new_thread();
-
-    let mut wasi_env = WasiEnv::builder("add_one").finalize(&mut store).unwrap();
-
-    let import_object = wasi_env
-        .import_object_for_all_wasi_versions(&mut store, &module)
+    let (instance, _) = WasiEnv::builder("add_one")
+        .instantiate(module, &mut store)
         .unwrap();
-    let instance = Instance::new(&mut store, &module, &import_object).unwrap();
 
-    wasi_env.initialize(&mut store, instance.clone()).unwrap();
-
-    let add_one = instance.exports.get_function("add_one").unwrap();
-    let result = add_one.call(&mut store, &[Value::I32(42)]).unwrap();
-    println!("{} + {} = {}", 42, 1, result[0].i32().unwrap()); //assert_eq!(, Value::I32(43));
+    let add_one: wasmer::TypedFunction<i32, i32> = instance
+        .exports
+        .get_typed_function(&mut store, "add_one")
+        .unwrap();
+    let result = add_one.call(&mut store, 42).unwrap();
+    println!("{} + {} = {}", 42, 1, result);
 }
