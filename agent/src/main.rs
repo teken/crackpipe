9298e64b23d7eb7db4de104284b1dc1ed2b2206c
Anna Haig <anna.haig@protonmail.com>
2023-06-10 22:27:52 +0100

    importing func

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,17 +1,19 @@
-use wasmer::{wat2wasm, Module, Store};
+use wasmer::{wat2wasm, Function, Instance, Module, Store};
 use wasmer_wasix::WasiEnv;
 
 fn main() {
     let wasm_bytes = wat2wasm(
         br#"
 (module
+    
+    (func $get_inc (import "env" "get_inc") (result i32))
     (memory (export "memory") 1)
-  (type $add_one_t (func (param i32) (result i32)))
-  (func $add_one_f (type $add_one_t) (param $value i32) (result i32)
-    local.get $value
-    i32.const 1
-    i32.add)
-  (export "add_one" (func $add_one_f)))
+    (type $add_one_t (func (param i32) (result i32)))
+    (func $add_one_f (type $add_one_t) (param $value i32) (result i32)
+        local.get $value
+        (call $get_inc)
+        i32.add)
+    (export "add_one" (func $add_one_f)))
 "#,
     )
     .unwrap();
@@ -20,9 +22,24 @@ fn main() {
 
     let module = Module::new(&store, wasm_bytes).unwrap();
 
-    let (instance, _) = WasiEnv::builder("add_one")
-        .instantiate(module, &mut store)
+    let mut wasi_env = WasiEnv::builder("add_one").finalize(&mut store).unwrap();
+
+    let mut import_object = wasi_env
+        .import_object_for_all_wasi_versions(&mut store, &module)
         .unwrap();
+
+    import_object.define(
+        "env",
+        "get_inc",
+        Function::new_typed(&mut store, || -> i32 {
+            println!("get_inc called");
+            1
+        }),
+    );
+
+    let instance = Instance::new(&mut store, &module, &import_object).unwrap();
+
+    wasi_env.initialize(&mut store, instance.clone()).unwrap();
 
     let add_one: wasmer::TypedFunction<i32, i32> = instance
         .exports
